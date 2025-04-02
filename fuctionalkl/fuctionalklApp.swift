7e0c3ca5418e21bb2b8d841f0c10c3f04b0555ad
Brandon Lee <bkl46@case.edu>
2025-04-01 23:09:00 -0400

    init

---
+++ fuctionalkl/fuctionalklApp.swift
@@ -0,0 +1,17 @@
+//
+//  fuctionalklApp.swift
+//  fuctionalkl
+//
+//  Created by Brandon Lee on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct fuctionalklApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fuctionalklTests/fuctionalklTests.swift
@@ -0,0 +1,17 @@
+//
+//  fuctionalklTests.swift
+//  fuctionalklTests
+//
+//  Created by Brandon Lee on 4/1/25.
+//
+
+import Testing
+@testable import fuctionalkl
+
+struct fuctionalklTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
